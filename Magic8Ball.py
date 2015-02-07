e45f88d7dff53e2cd46b4192a3e1b464dd90190f
drewdoescode <drewdoesgamemaker@gmail.com>
2015-02-06 19:26:12 -0500

    save

---
+++ Magic8Ball.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+name = 'Drew
+
+
+
+print('are you ready to fail my quiz!')
+an = input('>')
+print('what is googles main ip?')
+
+if input == '74.125.224.72':
+	print('correct!')
+else:
+	print('nope wrong now try again!')
+
